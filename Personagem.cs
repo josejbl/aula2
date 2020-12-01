4a319e2a2501ef4ad50782b224529065285ac9b1
FIC <FIC@info.local>
2020-12-01 11:03:57 -0300

    first commit

---
+++ Personagem.cs
@@ -0,0 +1,27 @@
+namespace aula2
+{
+    public class Personagem
+    {
+        public string nome = "Personagem";
+
+        public int idade = 40;
+
+        public string armadura = "plasma radiante";
+
+        public string armas = "Martelo asteroide,Basuca gravitacional";
+
+        public int vidas = 3; 
+
+        public string Atacar(){
+            return "Ataque pulsante!";
+
+      
+        }
+             public string Armas(){
+             return "martelada boo,Missio para tempo";
+        }
+
+
+
+    }
+}
